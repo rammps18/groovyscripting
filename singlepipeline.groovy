d7eac83d047d6801696921c9ad85bd69164ca0cc
Mohan <ram.karthik04@gmail.com>
2019-03-08 16:24:19 +0000

    updated

--- singlepipeline.groovy
+++ singlepipeline.groovy
@@ -1,21 +1,12 @@
-#!groovy
-import groovy.lang.Binding
 
-// Call shared libaray for common params
-def paramList = jobParams.listParams ([
-    "var1": "value",
-    "var2": "value2"
-])
-// Define repo specific params
-def addtionalParams = [
-    booleanParam(defaultValue: false, name: 'SOMETHING', description: 'description?'),
-    booleanParam(defaultValue: false, name: 'SOMETHING_ELSE', description: 'description?'),
-]
-// Set Jenkins job properties, combining both
-properties([
-    buildDiscarder(logRotator(numToKeepStr: '20')),
-    parameters(paramList + addtionalParams)
-])
+// Fields applied to all the environments
+
+def props() {
+	println("Hello World");
+}
+
+
+
 
 
 
