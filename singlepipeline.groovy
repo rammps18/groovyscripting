c0954b9ba18fd1e820ac16082646fc0885b9ed5d
rammps18 <rammps18@users.noreply.github.com>
2019-03-08 17:08:53 +0530

    Create singlepipeline.groovy

---
+++ singlepipeline.groovy
@@ -0,0 +1,9 @@
+
+// Fields applied to all the environments
+
+newParameters.Add([
+$class 			: 'StringParameterDefinition',
+name 			: 'RELEASE_TAG_NAME',
+defaultValue	: '',
+description		: SVN tag to use / deploy
+])
