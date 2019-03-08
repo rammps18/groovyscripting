8ad244bf8eba68907e0809606cbc4019c85780ab
Mohan <ram.karthik04@gmail.com>
2019-03-08 15:16:54 +0000

    updated

--- singlepipeline.groovy
+++ singlepipeline.groovy
@@ -2,14 +2,13 @@
 // Fields applied to all the environments
 
 def props() {
-def newParameters = []
-
-newParameters.Add([
-$class 			: 'StringParameterDefinition',
-name 			: 'RELEASE_TAG_NAME',
-defaultValue	: 'hello',
-description		: 'SVN tag to use / deploy',
-])
+	def newParameters = []
+		newParameters.Add([
+		$class 			: 'StringParameterDefinition',
+		name 			: 'RELEASE_TAG_NAME',
+		defaultValue	: 'hello',
+		description		: 'SVN tag to use / deploy',
+	])
 
 parameters(newParameters)
 
