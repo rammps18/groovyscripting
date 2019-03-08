1cb8afce403258d73aa90b96bd90660d1fdf643d
rammps18 <rammps18@users.noreply.github.com>
2019-03-08 12:19:00 +0000

    Update singlepipeline.groovy

--- singlepipeline.groovy
+++ singlepipeline.groovy
@@ -1,9 +1,17 @@
 
 // Fields applied to all the environments
 
+def props() {
+def newParameters = []
+
 newParameters.Add([
 $class 			: 'StringParameterDefinition',
 name 			: 'RELEASE_TAG_NAME',
 defaultValue	: '',
-description		: 'SVN tag to use / deploy',
+description		: SVN tag to use / deploy
 ])
+
+}
+
+
+
