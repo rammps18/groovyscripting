ca160611bfcc9f271f7ebd94e5f48d81b862a551
Mohan <ram.karthik04@gmail.com>
2019-03-08 16:45:47 +0000

    updated

--- firstprogram.groovy
+++ firstprogram.groovy
@@ -6,7 +6,7 @@ def pplnjob
 //load and define the pipeline
 node('master') {
 
-pplnjob = load '../workspace@script/singlepipeline.groovy'
+pplnjob = load './singlepipeline.groovy'
 pplnjob.props()
 
 }
