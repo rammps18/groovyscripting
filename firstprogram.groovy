f9a042f4854974575f0e6ebaa58ec052f6e962c8
Mohan <ram.karthik04@gmail.com>
2019-03-08 16:56:31 +0000

    updated

--- firstprogram.groovy
+++ firstprogram.groovy
@@ -1,12 +1,10 @@
-@Library('pipeline-shared-library@v5')
-
 //variables
 def pplnjob
 
 //load and define the pipeline
 node(master) {
 
-pplnjob = load '../workspace@script/singlepipeline.groovy'
+pplnjob = load "..\\workspace@script\\singlepipeline.groovy"
 pplnjob.props()
 
 }
