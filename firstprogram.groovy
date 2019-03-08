8cc5caeb14cb3ddd27c86153c5ab62c193d31bef
rammps18 <rammps18@users.noreply.github.com>
2019-03-08 12:18:29 +0000

    Update firstprogram.groovy

--- firstprogram.groovy
+++ firstprogram.groovy
@@ -3,7 +3,8 @@ import com.cloudbees.groovy.cps.NonCPS
 //load and define the pipeline
 node('master') {
 
-pplnjob = load '../workspace@script/Deploy_pipeline.groovy
+pplnjob = load '../workspace@script/jenkins_script/Deploy_pipeline.groovy
 pplnjob.props()
 
 }
+
