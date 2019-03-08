2fc765e7109d280722f2420bef6748e91de029ef
rammps18 <rammps18@users.noreply.github.com>
2019-03-08 17:01:44 +0530

    Create firstprogram.groovy

---
+++ firstprogram.groovy
@@ -0,0 +1,9 @@
+import com.cloudbees.groovy.cps.NonCPS
+
+//load and define the pipeline
+node('master') {
+
+pplnjob = load '../workspace@script/jenkins_script/Deploy_pipeline.groovy
+pplnjob.props()
+
+}
