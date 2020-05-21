619b09e302654dfca44d78fb3b0604b3124c28f7
rammps18 <rammps18@users.noreply.github.com>
2020-05-21 08:08:43 +0530

    command line groovy sh

---
+++ groovysh.groovy
@@ -0,0 +1,10 @@
+class Person {
+   def SayHello() { println "Hello" }
+}
+person = new Person()
+person.SayHello()
+
+// Just function
+def Hello(name) { println "Hello $name" }
+Hello("Mohan")
+   
